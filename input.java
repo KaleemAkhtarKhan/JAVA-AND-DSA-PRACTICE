2d7abf55e815c054e4be333322300e748f7bd250
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 01:48:11 +0530

    input in java

---
+++ input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        String input = Sc.next();
+        System.out.println(input); 
+        int number = Sc.nextInt();
+        System.out.println(number);
+        boolean price = true;
+        System.out.println(price);
+        float value = Sc.nextFloat();
+        System.out.println(value);
+        // for printing full line 
+        String name = Sc.nextLine();
+        System.out.println(name);
+        
+    }
+}
