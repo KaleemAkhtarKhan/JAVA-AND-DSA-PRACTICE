87ed6d026b568573676760491fb694bac314decd
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 01:04:55 +0530

    datatype

--- dataType.java
+++ dataType.java
@@ -2,5 +2,9 @@ public class dataType {
     public static void main(String[] args){
         byte a=8;
         System.out.println(a);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean Var =true;
+        System.out.println(Var);
     }
 }
