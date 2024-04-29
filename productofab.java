7a4a16b19ad3533b3aa48ce13a63c88228f87147
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 02:21:05 +0530

    multiplication of a and b

---
+++ productofab.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class productofab {
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int productofab = a*b;
+        System.out.println(productofab);
+    }
+}
+
