79b1c4b2bedf1fc75e4997ba7304738dc4760159
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 01:15:41 +0530

    sum of number

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+    // declaration of variable  
+        int a= 10;
+        int b= 5;
+    /*sum of number a and b (for multiline comment) */
+        int sum= a+b;
+        System.out.println(sum);
+    }
+}
+
