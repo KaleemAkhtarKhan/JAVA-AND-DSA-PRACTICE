e80460ae0a6db6705eb18a6e6e8dec6c0c12dc73
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-29 20:06:48 +0530

    pattern in java

---
+++ pattern1.java
@@ -0,0 +1,8 @@
+public class pattern1 {
+    public static void main(String[] args){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
