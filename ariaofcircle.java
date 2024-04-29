6c022de4b1bebc5ef549c2eabcb1b2d8c9161f64
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 02:27:44 +0530

    Aria of circle

---
+++ ariaofcircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ariaofcircle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int r = sc.nextInt();
+        double aria = 3.14*r*r;
+        System.out.println(aria);
+    }
+}
