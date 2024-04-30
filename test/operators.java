84c36d0648b44d89256d1fe5755c7a9d4dfadc0d
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 22:55:39 +0530

    operators

---
+++ test/operators.java
@@ -0,0 +1,12 @@
+public class operators{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+         System.out.println(a==b);
+         System.out.println(a<b);
+         System.out.println(a+b);
+         System.out.println(a%b);
+         System.out.println(a=b);
+
+    }
+}
