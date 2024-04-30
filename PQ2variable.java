d283f5762ac56fdf4a342fb1b2866e3a8736f92f
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 16:18:08 +0530

    practice question 2 of variable

---
+++ PQ2variable.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PQ2variable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float area = sc.nextFloat();
+        float areaofsquare = area*area;
+        System.out.println(areaofsquare);
+    }
+}
