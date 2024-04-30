3297f2578b69dc1df5c4bb0e470dff1b5e3a041b
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-05-01 01:21:58 +0530

    practice question

---
+++ Pconditinal.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pconditinal {
+    public static void main(String[] args) {
+        int a , b ;
+        Scanner sc = new Scanner(System.in);
+         a = sc.nextInt();
+         b = sc.nextInt();
+         sc.close();
+        if(a>b){
+            System.out.println("a is greater than b");
+
+        }
+    else{
+        System.out.println("b is grater than a ");
+    }
+
+
+    }
+}
