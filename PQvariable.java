c622daa999f1ea1eb1b726e9aff001e8e43f8f0d
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-04-30 16:12:59 +0530

    practice question

---
+++ PQvariable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PQvariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        float sum = a+b+c;
+        float average = sum/3;
+        System.out.println(average);
+    }
+}
