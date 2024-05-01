a53381c20c8a58a3a3344ca7ce67b3c81eac2b20
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-05-01 18:24:21 +0530

    practice on loop in java

---
+++ p4loop.java
@@ -0,0 +1,9 @@
+public class p4loop {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i<=4 ; i++){
+            System.out.println("****");
+        }
+            
+    }
+}
