3e918aaac072efe95808b49d05f469a3b5cedde3
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-05-02 19:10:03 +0530

    practice

---
+++ PQ2loop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PQ2loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter your number ");
+        int number = sc.nextInt();
+        sc.close();
+        System.out.println(number);
+        int sum = 0 ;
+        for ( int i=0; i<=number ; i++){
+            sum = sum + i;
+           
+        } System.err.println("sum of number is : "+sum );
+
+
+}
+    }
+    
---
+++ PQ3loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PQ3loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter the number :");
+        int num = sc.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= num; i++){
+            
+            factorial *=i;
+        }
+        System.out.println("factorial is "+factorial);
+    }
+}
