46caa1efa3514c954409139028a1679b9d1f3b68
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-05-03 16:22:53 +0530

    paratice on function in java

---
+++ function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class function{
+    public static int  claculatesum(int a ,int b){
+        int sum = a + b;
+        
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter your first number :");
+        int a = sc.nextInt();
+        System.out.print("enter your seccond number :");
+        int b = sc.nextInt();
+       int  sum = claculatesum(a,b);
+        System.out.println("sum is :" +sum);
+        claculatesum(a,b);
+        sc.close();
+    }
+}
