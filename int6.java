0fdc0d6a66521ad8f7e56d972b7442c44c6e500d
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-17 17:50:29 +0530

    practice 6

--- int6.java
+++ int6.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class int6 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double temp = sc.nextDouble();
+
+        if (temp > 100){
+            System.out.println("you have fever !!");
+        }
+        else{
+            System.out.println("you don't have fever !");
+        }
+        sc.close();
+    }
 }
