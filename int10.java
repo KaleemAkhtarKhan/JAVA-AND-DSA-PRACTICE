b186add97ef1fc4fbc0f01575e364c3b95f5a494
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-17 19:04:53 +0530

    pq

---
+++ int10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class int10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter the number for table :");
+        int num = sc.nextInt();
+        int i;
+        for ( i=1 ; i <=10; i++){
+            System.out.println( +num +"*" +i+ ":" +num*i);
+        }
+        sc.close();
+
+    }
+}
