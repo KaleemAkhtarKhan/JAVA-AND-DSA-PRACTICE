459f6636d8515bb63c2d2a4cd85c5eb54226f395
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-18 20:00:24 +0530

    java practice

---
+++ int22.java
@@ -0,0 +1,13 @@
+public class int22 {
+    public static void main(String[] args) {
+        char ch= 'A';
+        for(int i=1; i<=4; i++){
+            for(int  cha=1; cha<=i; cha++){
+                System.out.print(" "+ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+}
