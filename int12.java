8462580552103965e016cf3192365a7a9b6d71d3
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-18 16:46:09 +0530

    practice

---
+++ int12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class int12 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter the number :");
+        int num = sc.nextInt();
+        for(int i =1; i<=num ; i++){
+            System.out.println(i);
+        }
+
+
+    }
+}
