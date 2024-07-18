641e848a2edf4363b8569daf8019088d2b46aaac
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-18 19:17:44 +0530

    45

---
+++ int181.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class int181 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter the number :");
+        int num = sc.nextInt();
+
+            boolean isprime=true;
+            for( int i=2; i<=num; i++){
+
+                if(num%i==0){
+                    isprime=false;
+                }
+            }
+
+            if(isprime==true){
+                System.out.println("the number is not  a prime number !!");
+            } else{
+                System.out.println("the number is  a prime number!!");
+            }
+            sc.close();
+
+    }
+}
