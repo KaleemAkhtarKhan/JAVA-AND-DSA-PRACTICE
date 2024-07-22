663ba54f6b12a98e1471d26801f47e31e0a96ed6
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-07-22 22:26:38 +0530

    array

---
+++ intarray4.java
@@ -0,0 +1,20 @@
+public class intarray4 {
+ 
+    public static void pairs(int num[]){
+        for(int i= 0; i<num.length; i++){
+            int current = num[i];
+            for(int j= i; j<num.length; j++){
+                System.out.print("(" +current+ ","+ num[j]+" )");
+            }
+            System.out.println();
+        }
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        int num[] = {2, 4, 6, 8, 10}; 
+        pairs(num);
+    }
+}
