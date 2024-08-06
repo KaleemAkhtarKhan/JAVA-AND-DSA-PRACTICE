a3dc7bde37665f0bff05ec62a364bf3d239d321b
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-08-06 15:37:04 +0530

    array

--- array11.java
+++ array11.java
@@ -1,28 +1,27 @@
 public class array11 {
     public static int trapedwater(int height[]){
-        
+
         // calculate left max boundery
         int leftmax[] = new int[height.length];
         leftmax[0] = height[0];
         for(int i= 1; i<height.length; i++){
-            leftmax[i] = Math.max(height[i], height[i-1]); 
+            leftmax[i] = Math.max(height[i], leftmax[i-1]); 
         }
 
        // calculate right max boundary
         int rightmax[] = new int[height.length];
         rightmax [height.length-1] = height[height.length-1];
         for (int i=height.length-2; i>=0; i--){
-            rightmax[i] = Math.max(height[i], height[i+1]);
+            rightmax[i] = Math.max(height[i], rightmax[i+1]);
         }
 
 
         // loop
         int trapedwater = 0;
         for(int i = 0; i<height.length; i++){
-            int waterlevel = 0;
 
             // waterlevel 
-            waterlevel = Math.min(leftmax[i], rightmax[i]);
+             int waterlevel = Math.min(leftmax[i], rightmax[i]);
             
 
             // trapedwater
