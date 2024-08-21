bd992013766e28331bad774a3b5a7196def1f66e
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-08-22 01:30:38 +0530

    logic building

---
+++ int53.java
@@ -0,0 +1,12 @@
+public class int53 {
+    public static void main(String[] args) {
+        int num = 1;
+        for (int i = 0; i<= 5; i++){
+            for (int j= 1; j<=i; j++){
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ int54.java
@@ -0,0 +1,30 @@
+public class int54 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=5; i++){
+            for (int j= 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for (int j=1; j<=2*(5-i); j++){
+                System.out.print(" ");
+            }
+            for (int j= 1; j<=i; j++){
+                System.out.print("*");
+            }   
+            System.out.println(); 
+        }
+
+        // to print inverceof upper pattern
+        for(int i = 5; i>=1; i--){
+            for (int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+            for (int j=1; j<=2*(5-i); j++){
+                System.out.print(" ");
+            }
+            for (int j=i; j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
