98ad4bfddf4bf92ea84fb9e450d5c78a72ec31e1
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-08-28 00:09:40 +0530

    string problem

---
+++ string4.java
@@ -0,0 +1,28 @@
+public class string4 {
+    public static float getshortestpath(String path){
+        int x= 0; 
+        int y =0;
+        for(int i = 0; i<path.length(); i++){
+            int direction = path.charAt(i);
+            if(direction =='N'){
+                y = y+1;
+                
+            } else if(direction =='S'){
+                y = y-1;
+            } else if(direction=='W'){
+                x= x-1;
+            }else{
+                x= x+1;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        // formula for calculating direction 
+        Float result =(float) Math.sqrt(X2+Y2);
+        return result;
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getshortestpath(path));
+    }
+}
