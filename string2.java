9ba3d12b429075abd078c5304a85c5670f2dfa25
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-08-27 19:43:22 +0530

    string  updated

---
+++ string2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class string2 {
+    public static void main(String[] args) {
+         // string input in java
+    Scanner sc = new Scanner (System.in);
+    System.out.print("enter the first name :");
+    String firstname = sc.next();
+    System.out.print("enter the lastname:");
+    String lastname = sc.next();
+    // concatanation of string (addition of multiple string )
+    String fullname = firstname + " "+ lastname;
+    System.out.println(fullname);
+    }
+}
