d872bf9a8c11f2d8ff60b1b930a7ba679ab610a4
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2025-04-16 07:05:10 +0530

    tcs

---
+++ tcs2.java
@@ -0,0 +1,11 @@
+public class tcs2 {
+    public static void main(String[] args) {
+        // sum of first n natural numbers 
+        int n= 5;
+        int sum= 0;
+        for(int i=1; i<=n; i++){
+            sum = sum+i;
+        }
+        System.out.println("sum of"+n+"number is :"+sum);
+    }
+}
