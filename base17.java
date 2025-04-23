f4e7401498c5dc26ef2038650eb5209ca3ec6b00
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2025-04-23 21:23:56 +0530

    ghvhg

--- base17.java
+++ base17.java
@@ -22,6 +22,7 @@ public class base17 {
             trapedWter = trapedWter+ waterlevel-height[i];
         }
         return trapedWter;
+        
     }
     public static void main(String[] args) {
         int height[] = {4, 2, 0, 6, 3, 2, 5};
