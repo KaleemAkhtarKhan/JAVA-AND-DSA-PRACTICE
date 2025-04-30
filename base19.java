b646e9a5d7a73f76b22e33d86ecf2c821a5fef70
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2025-04-30 18:04:12 +0530

    practice

--- base19.java
+++ base19.java
@@ -1,4 +1,11 @@
 public class base19 {
+    public static void buyandsell(int prices[]){
+        int maxprofit = Integer.MIN_VALUE;
+        int profit = 0;
+        for(int i = 0; i<prices.length; i++){
+            profit = 0;
+        }
+    }
     public static void main(String[] args) {
         int prices[] = {7, 1, 5, 3, 6, 4};
         
